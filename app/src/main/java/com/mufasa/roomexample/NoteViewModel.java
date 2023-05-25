ab80f561ab23d1d9a6af8d2f05f7ac44b532edf3
mustafaariwala <ariwalamustafa853@gmail.com>
2023-05-25 14:45:29 +0530

    Initialized database, Note class, and Note Repository

---
+++ app/src/main/java/com/mufasa/roomexample/NoteViewModel.java
@@ -0,0 +1,24 @@
+package com.mufasa.roomexample;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository noteRepository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        noteRepository = new NoteRepository(application);
+        allNotes = noteRepository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        noteRepository.insert(note);
+    }
+}
