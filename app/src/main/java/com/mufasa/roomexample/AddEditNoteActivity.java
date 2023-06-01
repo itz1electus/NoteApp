14bcb0e572ed970d832b49f99ce8e08350d22b31
mustafaariwala <ariwalamustafa853@gmail.com>
2023-06-01 13:24:51 +0530

    Added functionality to update existing notes and added animations for insert, update and delete

--- app/src/main/java/com/mufasa/roomexample/AddEditNoteActivity.java
+++ app/src/main/java/com/mufasa/roomexample/AddEditNoteActivity.java
@@ -12,10 +12,11 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
-public class AddNoteActivity extends AppCompatActivity {
+public class AddEditNoteActivity extends AppCompatActivity {
     public static final String EXTRA_TITLE = "com.mufasa.roomexample.EXTRA_TITLE";
     public static final String EXTRA_DESCRIPTION = "com.mufasa.roomexample.EXTRA_DESCRIPTION";
     public static final String EXTRA_PRIORITY = "com.mufasa.roomexample.EXTRA_PRIORITY";
+    public static final String EXTRA_ID = "com.mufasa.roomexample.EXTRA_ID";
 
     private EditText editTextTitle;
     private EditText editTextDescription;
@@ -34,7 +35,16 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPickerPriority.setMaxValue(10);
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        setTitle("Add Note");
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_ID)) {
+            setTitle("Edit Note");
+            editTextTitle.setText(intent.getStringExtra(EXTRA_TITLE));
+            editTextDescription.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            numberPickerPriority.setValue(intent.getIntExtra(EXTRA_PRIORITY, 1));
+        } else {
+            setTitle("Add Note");
+        }
     }
 
     private void saveNote() {
@@ -51,6 +61,11 @@ public class AddNoteActivity extends AppCompatActivity {
         data.putExtra(EXTRA_TITLE, title);
         data.putExtra(EXTRA_DESCRIPTION, description);
         data.putExtra(EXTRA_PRIORITY, priority);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id != -1) {
+            data.putExtra(EXTRA_ID, id);
+        }
 
         setResult(RESULT_OK, data);
         finish();
